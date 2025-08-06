7ea2a8f78e38635b9d5b7c3f33c03f36e1d2ad8b
Lindsey Bieda <hazardous.narayan@gmail.com>
2025-08-05 22:35:08 -0700

    Add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,3 @@
-var grid = document.querySelector('.grid');
-
-if (grid) {
-  var msnry = new Masonry( grid, {
-    itemSelector: '.grid-item',
-    columnWidth: 50,
-    fitWidth: true,
-    gutter: 3,
-    stagger: 30,
-  });
-}
-
-function addMoonAPIWidget() {
-  var btn = document.createElement('button');
-  btn.innerText = '🌓'; // Moon Icon
-  btn.title = 'Check the Moon Phase';
-  btn.style.position = 'fixed';
-  btn.style.bottom = '10px';
-  btn.style.right = '10px';
-  btn.style.zIndex = '9999';
-  btn.style.background = '#ffffff';
-  btn.style.color = '#f8f8f2';
-  btn.style.borderRadius = '50%';
-  btn.style.width = '48px';
-  btn.style.height = '48px';
-  btn.style.fontSize = '24px';
-  btn.style.cursor = 'pointer';
-  btn.style.display = 'flex';
-  btn.style.alignItems = 'center';
-  btn.style.justifyContent = 'center';
-  btn.style.border = 'none';
-  btn.addEventListener('click', function() {
-    window.open('https://moon-api.co/', '_blank'); // Open in new tab
-  });
-  document.body.appendChild(btn);
-}
-
-
 function addLastFmWidgetToDiv(divSelector, username, apiKey) {
   var targetDiv = document.querySelector(divSelector);
   if (!targetDiv) return;
@@ -88,5 +50,3 @@ function addLastFmWidgetToDiv(divSelector, username, apiKey) {
 window.addEventListener('load', function() {
   addLastFmWidgetToDiv('#lastfm-widget', 'BeEkkoDoCrimes', '421d1d180d48e1c30fcb3ec771bba221'); // please don't abuse my API key
 });
-
-window.addEventListener('load', addMoonAPIWidget);
